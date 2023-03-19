ade6d2b017ddbe1cbbd1b87e761c4ccf35415b71
soohykeee <gur1028@skuniv.ac.kr>
2023-03-19 15:25:49 +0900

    fix : pets에 enum 클래스로 생성한 types 추가

--- src/main/java/kr/co/jshpetclinicstudy/persistence/entity/Pets.java
+++ src/main/java/kr/co/jshpetclinicstudy/persistence/entity/Pets.java
@@ -24,14 +24,21 @@ public class Pets {
     private LocalDateTime birth_date;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "owner_id")
     private Owners owners;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="type_id")
+    private Types types;
 
     @Builder
     public Pets(String name,
                 LocalDateTime birth_date,
-                Owners owners) {
+                Owners owners,
+                Types types) {
         this.name = name;
         this.birth_date = birth_date;
         this.owners = owners;
+        this.types = types;
     }
 }
