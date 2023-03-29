2b8e8bb0a91d6d16278be66bfd24d67bf9604468
soohykeee <gur1028@skuniv.ac.kr>
2023-03-29 21:32:00 +0900

    refactor : 연관관계 수정

--- src/main/java/kr/co/jshpetclinicstudy/persistence/entity/Vets.java
+++ src/main/java/kr/co/jshpetclinicstudy/persistence/entity/Vets.java
@@ -1,8 +1,12 @@
 package kr.co.jshpetclinicstudy.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AttributeOverride(name = "id", column = @Column(name = "vet_id", length = 4))
@@ -17,16 +21,14 @@ public class Vets extends BaseEntity{
     @Column(name = "last_name", length = 30)
     private String lastName;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name="vets_specialties")
-    private Specialties specialties;
+    @OneToMany(mappedBy = "vets")
+    private List<VetsSpecialties> vets = new ArrayList<>();
 
     public Vets(String firstName,
                 String lastName,
-                Specialties specialties) {
+                List<VetsSpecialties> vets) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.specialties = specialties;
+        this.vets = vets;
     }
-
 }
