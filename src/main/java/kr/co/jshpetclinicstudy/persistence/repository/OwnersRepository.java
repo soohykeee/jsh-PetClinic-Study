10529fa0a790343be18855c2c799326e9364c09b
soohykeee <gur1028@skuniv.ac.kr>
2023-03-31 20:23:05 +0900

    feat : owners id find by telephone

--- src/main/java/kr/co/jshpetclinicstudy/persistence/repository/OwnersRepository.java
+++ src/main/java/kr/co/jshpetclinicstudy/persistence/repository/OwnersRepository.java
@@ -2,9 +2,14 @@ package kr.co.jshpetclinicstudy.persistence.repository;
 
 import kr.co.jshpetclinicstudy.persistence.entity.Owners;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OwnersRepository extends JpaRepository<Owners, Long> {
 
+    @Query("select Owners.id " +
+            "from Owners " +
+            "where Owners.telephone=: teleephone")
+    Long findOwnerIdByTelephone(String telephone);
 }
