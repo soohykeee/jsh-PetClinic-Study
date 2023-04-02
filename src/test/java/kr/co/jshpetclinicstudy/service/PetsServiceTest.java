041c7a3aaf32c8655ceb5e5b576d5421eca0a83b
soohykeee <gur1028@skuniv.ac.kr>
2023-04-02 18:44:55 +0900

    Revert "refactor : modify layer structure"
    This reverts commit 9bdd49f0aa222d535f9ebade22a23ed830f7dea3.

--- src/test/java/kr/co/jshpetclinicstudy/service/PetsServiceTest.java
+++ src/test/java/kr/co/jshpetclinicstudy/service/PetsServiceTest.java
@@ -1,20 +1,15 @@
 package kr.co.jshpetclinicstudy.service;
 
-import kr.co.jshpetclinicstudy.persistence.entity.Owners;
-import kr.co.jshpetclinicstudy.persistence.entity.Pets;
-import kr.co.jshpetclinicstudy.persistence.repository.OwnersRepository;
+import kr.co.jshpetclinicstudy.persistence.dto.PetsDto;
 import kr.co.jshpetclinicstudy.persistence.repository.PetsRepository;
-import kr.co.jshpetclinicstudy.service.model.dtos.PetsRequestDto;
-import kr.co.jshpetclinicstudy.service.model.dtos.PetsResponseDto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PetsServiceTest {
@@ -23,65 +18,37 @@ class PetsServiceTest {
     private PetsService petsService;
     @Autowired
     private PetsRepository petsRepository;
-    @Autowired
-    private OwnersRepository ownersRepository;
 
     @Test
-    void createPet() {
-        Optional<Owners> owners = ownersRepository.findOwnerByTelephone("01064564655");
-
-        PetsRequestDto.CREATE create = PetsRequestDto.CREATE.builder()
+    void createPets() {
+        PetsDto dto = PetsDto.builder()
                 .name("멍멍이")
-                .birthDate(LocalDate.of(2021, 1, 9))
-                .owners(owners.get())
+                .birthDate(LocalDate.ofYearDay(2022, 1))
+                .ownerFirstName("수혁")
+                .ownerTelephone("01064564655")
                 .type("푸들")
                 .build();
 
-        petsService.createPet(create);
-        Optional<Pets> pets = petsRepository.findById(1L);
-        assertThat(pets.get().getName()).isEqualTo("멍멍이");
+        petsService.createPets(dto);
     }
 
     @Test
-    void getPetList() {
-        List<PetsResponseDto.READ> readList = petsService.getPetList();
+    void getPets() {
+        PetsDto petsDto = petsService.getPets(1L);
 
-        assertThat(readList.get(0).getName()).isEqualTo("멍멍이");
-        assertThat(petsRepository.findAll().size()).isEqualTo(readList.size());
+        Assertions.assertThat(petsDto.getName()).isEqualTo("멍멍이");
     }
 
     @Test
-    void getPet() {
-        PetsResponseDto.DETAIL_READ detailRead = petsService.getPet(2L);
-        assertThat(detailRead.getName()).isEqualTo("뭉이");
-        assertThat(detailRead.getOwnerFirstName()).isEqualTo("수혁");
+    void updatePets() {
+
     }
 
     @Test
-    void getPetListOfOwner() {
-        List<PetsResponseDto.READ> petListOfOwner = petsService.getPetListOfOwner(1L);
+    void deletePets() {
+        petsService.deletePets(1L);
 
-        assertThat(petListOfOwner.get(2).getName()).isEqualTo("초코");
-        assertThat(petListOfOwner.size()).isEqualTo(3);
-    }
-
-    @Test
-    void updatePet() {
-        PetsRequestDto.UPDATE update = PetsRequestDto.UPDATE.builder()
-                .petId(3L)
-                .name("딸기")
-                .birthDate(LocalDate.of(2011, 01, 01))
-                .type("포메라니안")
-                .build();
-
-        petsService.updatePet(update);
-        assertThat(petsRepository.findById(3L).get().getName()).isEqualTo("딸기");
-    }
-
-    @Test
-    void deletePet() {
-        petsService.deletePet(3L);
-        assertThat(petsRepository.findById(3L)).isEmpty();
+        Assertions.assertThat(petsRepository.findById(1L).isEmpty()).isTrue();
     }
 
 }
