67c2aac5a2327a9ea635f6f0b320cbd24c778b4b
soohykeee <gur1028@skuniv.ac.kr>
2023-04-02 18:45:06 +0900

    Revert "fix : findOwnerByTelephone method change parameter, add nativeQuery"
    This reverts commit c9024566dfafe8c393355889c97feb2d6d71bbd3.

--- src/main/java/kr/co/jshpetclinicstudy/persistence/repository/OwnersRepository.java
+++ src/main/java/kr/co/jshpetclinicstudy/persistence/repository/OwnersRepository.java
@@ -10,9 +10,9 @@ import java.util.Optional;
 @Repository
 public interface OwnersRepository extends JpaRepository<Owners, Long> {
 
-    @Query(value = "select o.owner_id, o.address, o.city, o.first_name, o.last_name, o.telephone " +
-            "from tbl_owners o " +
-            "where o.telephone=:telephone", nativeQuery = true)
+    @Query("select Owners.id " +
+            "from Owners " +
+            "where Owners.telephone=: teleephone")
     Optional<Owners> findOwnerByTelephone(String telephone);
 
 }
