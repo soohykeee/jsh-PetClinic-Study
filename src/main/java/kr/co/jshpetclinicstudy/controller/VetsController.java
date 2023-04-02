867dba45201c7297374650a9a5197848d7de4901
soohykeee <gur1028@skuniv.ac.kr>
2023-04-02 18:44:52 +0900

    Revert "refactor : vets crud + test code"
    This reverts commit 7c4c663a44213b6668c8746d25b464a0161bab08.

--- src/main/java/kr/co/jshpetclinicstudy/controller/VetsController.java
+++ src/main/java/kr/co/jshpetclinicstudy/controller/VetsController.java
@@ -1,43 +1,12 @@
 package kr.co.jshpetclinicstudy.controller;
 
-import jakarta.validation.Valid;
 import kr.co.jshpetclinicstudy.service.VetsService;
-import kr.co.jshpetclinicstudy.service.model.dtos.VetsRequestDto;
-import kr.co.jshpetclinicstudy.service.model.dtos.VetsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/vets/")
 public class VetsController {
 
     private final VetsService vetsService;
-
-    public void createVet(@RequestBody @Valid VetsRequestDto.CREATE create) {
-        vetsService.createVet(create);
-    }
-
-    public List<VetsResponseDto.READ> getVetList() {
-        return vetsService.getVetList();
-    }
-
-    public VetsResponseDto.DETAIL_READ getVet(Long id) {
-        VetsResponseDto.DETAIL_READ detailRead = vetsService.getVet(id);
-        return detailRead;
-    }
-
-    public void updateVet(@RequestBody @Valid VetsRequestDto.UPDATE update) {
-        vetsService.updateVet(update);
-    }
-
-    public void deleteVet(Long id) {
-        vetsService.deleteVet(id);
-    }
-
 }
--- src/main/java/kr/co/jshpetclinicstudy/persistence/entity/Vets.java
+++ src/main/java/kr/co/jshpetclinicstudy/persistence/entity/Vets.java
@@ -1,8 +1,6 @@
 package kr.co.jshpetclinicstudy.persistence.entity;
 
 import jakarta.persistence.*;
-import kr.co.jshpetclinicstudy.service.model.dtos.VetsRequestDto;
-import kr.co.jshpetclinicstudy.service.model.dtos.VetsResponseDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,47 +22,13 @@ public class Vets extends BaseEntity{
     private String lastName;
 
     @OneToMany(mappedBy = "vets")
-    private List<VetsSpecialties> specialties = new ArrayList<>();
+    private List<VetsSpecialties> vets = new ArrayList<>();
 
-    @Builder
     public Vets(String firstName,
                 String lastName,
-                List<VetsSpecialties> specialties) {
+                List<VetsSpecialties> vets) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.specialties = specialties;
-    }
-
-    public static Vets dtoToEntity(VetsRequestDto.CREATE create) {
-        return Vets.builder()
-                .firstName(create.getFirstName())
-                .lastName(create.getLastName())
-                .build();
-    }
-
-    public static VetsResponseDto.READ entityToDto(Vets vets) {
-        return VetsResponseDto.READ.builder()
-                .vetId(vets.getId())
-                .firstName(vets.getFirstName())
-                .lastName(vets.getLastName())
-                .specialties(vets.getSpecialties())
-                .build();
-    }
-
-    public static VetsResponseDto.DETAIL_READ entityToDetailDto(Vets vets) {
-        return VetsResponseDto.DETAIL_READ.builder()
-                .vetId(vets.getId())
-                .firstName(vets.getFirstName())
-                .lastName(vets.getLastName())
-                .specialties(vets.getSpecialties())
-                .build();
-    }
-
-    public void changeVetFirstName(String changeFirstName) {
-        this.firstName = changeFirstName;
-    }
-
-    public void changeVetLastName(String changeLastName) {
-        this.lastName = changeLastName;
+        this.vets = vets;
     }
 }
