d91d60cade01005fcc254656f25aff6c48d64f6d
soohykeee <gur1028@skuniv.ac.kr>
2023-04-02 18:45:21 +0900

    Revert "feat : owners crud + test code"
    This reverts commit 2f3420c0b2149e922657bb3761fffb7a5e5abba5.

--- src/main/java/kr/co/jshpetclinicstudy/controller/OwnersController.java
+++ src/main/java/kr/co/jshpetclinicstudy/controller/OwnersController.java
@@ -1,35 +1,13 @@
 package kr.co.jshpetclinicstudy.controller;
 
-import kr.co.jshpetclinicstudy.persistence.dto.OwnersDto;
 import kr.co.jshpetclinicstudy.service.OwnersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/owners")
 public class OwnersController {
 
     private final OwnersService ownersService;
 
-    @GetMapping("/list")
-    public void list() {
-
-    }
-
-    @GetMapping("/register")
-    public void register() {
-
-    }
-
-    @PostMapping("/register")
-    public String registerPost(OwnersDto dto) {
-        ownersService.createOwners(dto);
-
-        return "redirect:/api/v1/";
-    }
 }
