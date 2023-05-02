c89bbfe26e5cae3231908cadb9f899aff55cc2af
soohykeee <gur1028@skuniv.ac.kr>
2023-05-02 21:24:10 +0900

    refactor : add redDate, modDate -> use JPA Auditing

--- src/main/java/kr/co/jshpetclinicstudy/persistence/entity/BaseEntity.java
+++ src/main/java/kr/co/jshpetclinicstudy/persistence/entity/BaseEntity.java
@@ -1,17 +1,26 @@
 package kr.co.jshpetclinicstudy.persistence.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
+
+    @CreatedDate
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    private LocalDateTime modDate;
 
 }
