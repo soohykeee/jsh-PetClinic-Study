dcd55af0234a1b60dcaebf7fd3a60a0e9c3fa1dc
soohykeee <gur1028@skuniv.ac.kr>
2023-05-23 20:35:44 +0900

    feat : add spring security

---
+++ src/main/java/kr/co/jshpetclinicstudy/infra/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package kr.co.jshpetclinicstudy.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // jwt 사용을 해줄 것 이기에 csrf 비활성화
+        http
+                .csrf().disable();
+
+        // Authorization (인가)
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated();
+
+        // Authentication (인증)
+        http
+                .formLogin().disable();
+
+        return http.build();
+    }
+
+}
